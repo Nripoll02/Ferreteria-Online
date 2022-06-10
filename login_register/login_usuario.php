6d7e0602dea3c3122de0df0afdd0219715b574f4
Nahuel Ripoll <nahuelripoll33@gmail.com>
2022-06-10 16:10:15 -0300

    Proyecto global

--- login_register/login_usuario.php
+++ login_register/login_usuario.php
@@ -8,24 +8,20 @@ $contrasenia = $_POST['contrasenia'];
 $contrasenia = hash('sha512', $contrasenia);
 
 $validar_login = mysqli_query($conexion, "SELECT * FROM usuarios WHERE correo='$correo' and contrasenia='$contrasenia'");
-$roles=mysqli_fetch_array($validar_login);
 
-if($roles['id_cargo']==1)  {//Admin
-        $_SESSION['usuario'] = $correo;
-        header("location:../admin/inicio.php");
-    exit();
-}else if($roles['id_cargo']==2){//Lector
-        $_SESSION['usuario'] = $correo;
-        header("location:../pages/inicio.php");
-}
-else{
+if(mysqli_num_rows($validar_login) > 0){
+    header("location: ../pages/inicio.php");
+}else{
     echo'
-        <script>
-            alert("El usuario no existe o la contraseña es incorrecta");
-            window.location = "../index.php";
-        </script>
+    <script>
+        alert("Usuario o contraseña erroneo");
+        window.location= "../index.php";
+    </script>
+
     ';
     exit();
+
+
 }
 
 
